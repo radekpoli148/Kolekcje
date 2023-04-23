c1c5094f6a07c405340644a8721cb4b8fe87be26
Radosław Potyrała <radekpoli01480@op.pl>
2023-04-23 13:47:10 +0200

    poprawa działania programu, rozszerzenie o synchronizacje wielowątkowe

---
+++ src/kolekcjeitd/KolekcjeITD.java
@@ -0,0 +1,47 @@
+package kolekcjeitd;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+
+public class KolekcjeITD 
+{
+    public static void main(String[] args) 
+    {
+        Collection a;
+        ArrayList b;
+        /**
+         * w listach kolejność ma znaczenie
+         * 
+         * FF12   FF13   FF14
+         * get(0) get(1) get(2)
+         * 
+         * W zbiorach wielokrotność oraz kolejność występowania nie ma znaczenia
+         * 
+         * A = {1,1,1,1}
+         * B = {1}
+         * A = B
+         * 
+         * A = {2,4,5}
+         * B = {4,5,2}
+         * A = B
+         * 
+         * przyporządkowanie - mapowanie połączenie 2 elementów
+         * z dziedziny oraz przeciwdziedziny
+         * 
+         * KEY - VALUE
+         * ID 1 - "Ania"
+         * ID 2 - "Bartosz"
+         * ID 3 - "Celina"
+         * 
+         * C = {a,b}
+         * D = {c,d,e}
+         * 
+         * X = {a,c}; y = {b,e}
+         * 
+         */
+        Map c;
+    }
+    
+}
